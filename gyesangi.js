19d1d921f211b80d715f68dba5e862dd4f50f94b
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-08-01 11:57:55 +0900

    계산기 만들기

--- gyesangi.js
+++ gyesangi.js
@@ -15,3 +15,24 @@ class Calculator {
 
 const buttons = document.querySelectorAll('button')
 const displayElement = document.querySelector('input')
+const calculator = new Calculator(displayElement)
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        switch (button.dataset.type) {
+            case 'operator':
+                console.log('operator')
+                break
+            case 'ac':
+                console.log('ac')
+                break
+            case 'equals':
+                console.log('equals')
+                break
+            default:
+                calculator.appendNumber(button.innerText)
+                calculator.updateDisplay()
+                break
+        }
+    })
+})
