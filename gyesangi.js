082adbe8979b350b03ac42b234982da41c58677b
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-08-01 11:42:34 +0900

    계산기 만들기

--- gyesangi.js
+++ gyesangi.js
@@ -1,2 +1,17 @@
+class Calculator {
+    constructor(displayElement) {
+        this.displayElement = displayElement
+        this.displayContent = ''
+    }
+
+    appendNumber(number) {
+        this.displayContent += number
+    }
+
+    updateDisplay() {
+        this.displayElement.value = this.displayContent
+    }
+}
+
 const buttons = document.querySelectorAll('button')
 const displayElement = document.querySelector('input')
